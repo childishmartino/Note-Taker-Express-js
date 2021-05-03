10c0cd65c0464b14d8026c8046a8400674e61e52
childishmartino <76404189+childishmartino@users.noreply.github.com>
2021-05-02 18:21:06 -0700

    working code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require("./routing/api-routes")
+const htmlRoutes = require("./routing/html-routes")
+
+
+// when deploying to Heroku dont forget process.env.PORT look at heroku doc. 
+const PORT = process.env.PORT || 3001;
+
+//express app setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routing api info and front end info
+app.use(express.static("public"));
+app.use("/api", apiRoutes)
+app.use("/", htmlRoutes)
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
